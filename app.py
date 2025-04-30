029faebf6c860d3580b0f2ddf6c781a8e760a1df
Harman <harmandeep.singh233@lpu.in>
2025-04-30 17:00:43 +0530

    second commit

--- app.py
+++ app.py
@@ -135,4 +135,4 @@ def serve_file(download_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
